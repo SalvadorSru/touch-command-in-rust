1a6012a1e002c4f24e115f924ced6f91c482c935
salvadorsru <salvadorsru@gmail.com>
2024-08-02 13:34:41 +0200

    feat: add support for creating both directories and files

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,36 @@ fn main() {
     }
 
     // Get the file path from the arguments
-    let requested_file: &str = &args[1];
+    let requested_path: &str = &args[1];
 
-    // Create a Path object from the file path
-    let path = Path::new(requested_file);
+    // Create a Path object from the path
+    let path = Path::new(requested_path);
 
-    // Extract the parent directory from the path
-    let folders = path.parent().unwrap_or_else(|| Path::new(""));
+    // Check if the path is a file or directory based on its extension or trailing slashes
+    if path.extension().is_some()
+        || path.file_name().is_some() && path.file_name().unwrap().to_str().unwrap().contains('.')
+    {
+        // Path is assumed to be a file
+        // Extract the parent directory from the path
+        let folders = path.parent().unwrap_or_else(|| Path::new("."));
 
-    // Attempt to create necessary directories
-    if let Err(e) = create_dir_all(folders) {
-        eprintln!("Error creating directories: {}", e);
-        process::exit(1); // Exit with an error code
-    }
+        // Attempt to create necessary directories
+        if let Err(e) = create_dir_all(folders) {
+            eprintln!("Error creating directories: {}", e);
+            process::exit(1); // Exit with an error code
+        }
 
-    // Attempt to create the file; if it already exists, it will be opened
-    if let Err(e) = File::create(requested_file) {
-        eprintln!("Error creating file: {}", e);
-        process::exit(1); // Exit with an error code
+        // Attempt to create the file; if it already exists, it will be opened
+        if let Err(e) = File::create(path) {
+            eprintln!("Error creating file: {}", e);
+            process::exit(1); // Exit with an error code
+        }
+    } else {
+        // Path is assumed to be a directory
+        // Attempt to create the directory (and any necessary parent directories)
+        if let Err(e) = create_dir_all(path) {
+            eprintln!("Error creating directories: {}", e);
+            process::exit(1); // Exit with an error code
+        }
     }
 }
